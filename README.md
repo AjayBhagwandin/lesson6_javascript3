611d9d0468e813d23bfafbc00fe9809230c81203
Ajay Bhagwandin <ab17ad@my.fsu.edu>
2018-07-13 20:19:38 +0000

    Lesson 6 final commit

--- README.md
+++ README.md
@@ -4,19 +4,17 @@
 Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+7/13/2018)
 
 ### Student Name:  
-(First and Last Name)
+Ajay Bhagwandin
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
-
+We learned how to make a photo gallery
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+https://ajaybhagwandin.github.io/lesson6_javascript3/.
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. I learned to make a photo gallery
+2. learned to add images
+3. Learned about phto dimentions 
--- photos.js
+++ photos.js
@@ -1,7 +1,7 @@
 /*    
     Program Name:  Photo Gallery Application
-    Author: 
-    Date:   
+    Author: Ajay Bhagwandin
+    Date:   7/13
     Filename: photos.js
     
  */
@@ -17,11 +17,25 @@ var figureCount = 3;
 /* This method adds src values to img elements based on order specified in photoOrder array             */
 /********************************************************************************************************/
 function populateFigures() {
+    var filename;
+    var currentFig;
+    
+    if (figureCount === 3) {
+    for (var i = 1; i < 4; i++) {
+        filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+        currentFig = document.getElementsByTagName("img")[i -1];
+        currentFig.src = filename;
+    }
+
+} else {
+    for (var i = 0; i<5; i++) {
+        filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+        currentFig = document.getElementsByTagName("img")[i];
+        currentFig.src = filename;
 
 
 
-
-}//end of populateFigures function
+}}}//end of populateFigures function
 
 /**********************************END OF populateFigures() FUNCTION*************************************/
 
@@ -69,7 +83,37 @@ function leftArrow() {
 /******************************************************************************************************/
 function previewFive() {
    
-
+ var articleEl = document.getElementsByTagName("article")[0];
+ var lastFigure = document.createElement("figure");
+  lastFigure.id = "fig5";
+    lastFigure.style.zIndex = "5";
+    lastFigure.style.position = "abosolute";
+    lastFigure.style.right = "45px";
+    lastFigure.style.top = "67px";
+    
+     var lastImage = document.createElement("img");
+    lastImage.width = "240";
+    lastImage.height = "135";
+    
+    lastFigure.appendChild(lastImage);
+    
+    articleEl.appendChild(lastFigure);
+    
+     var firstFigure = lastFigure.cloneNode(true);
+    
+    firstFigure.id = "fig1";
+    firstFigure.style.right = " ";
+    firstFigure.style.left = "45px";
+    
+    articleEl.insertBefore(firstFigure, document.getElementById("fig2"));
+    
+      document.getElementsByTagName("img")[0].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
+    document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
+    
+    figureCount = 5;
+    
+        var numberButton = document.querySelector("#fiveButton p");
+    numberButton.removeEventListener("click", previewFive, false);
    
    
 }//end of previewFive()
@@ -83,6 +127,12 @@ function previewFive() {
 /*******************************************************************************************************/
 function createEventListeners() {
    
+var leftarrow = document.getElementById("leftarrow");
+leftarrow.addEventListener("click", leftArrow, false);
+var rightarrow = document.getElementById("rightarrow");
+rightarrow.addEventListener("click", rightArrow, false);
+   var showAllButton = document.querySelector("#fiveButton p");
+   showAllButton.addEventListener("click", previewFive, false);
 
 
    
